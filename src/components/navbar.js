2431d55a3e1dcc3ca5e05c1fe7113622f6b0c575
Fernando Renteria <fernandorenteria@Fernandos-MacBook-Pro.local>
2019-06-08 14:45:53 -0500

    Added Reat Router and initial nav component

---
+++ src/components/navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+    return(
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Memorizer</li>
+            </ul>
+        </nav>
+    );
+};
